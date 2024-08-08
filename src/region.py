037dcf05a763d3b97af354a135ca79659ba51a94
erwinlambert <erwin.lambert@knmi.nl>
2024-08-08 15:32:41 +0000

    timeseries in notebook

--- src/region.py
+++ src/region.py
@@ -9,11 +9,13 @@ from run import Run
 from run import dpm
 
 class Region:
-    def __init__(self, region,model,refmodel='UKESM1-0-LL'):
+    def __init__(self, region,model,refmodel='UKESM1-0-LL',k0=0,k1=6000):
         # Get volume CMIP
         self.region = region
         self.model = model
         self.refmodel = refmodel
+        self.k0 = k0
+        self.k1 = k1
 
         self.get_historical()
 
@@ -22,9 +24,14 @@ class Region:
         #self.get_woa()
         #self.get_woa()
         #self.ref = self.woa
-        self.ref = Run(self.refmodel,self.region,'historical',1995,2015)
-        self.prd = Run(self.model,self.region,'historical',1995,2015)
+        self.ref = Run(self.refmodel,self.region,'historical',1995,2015,k0=self.k0,k1=self.k1)
+        self.prd = Run(self.model,self.region,'historical',1995,2015,k0=self.k0,k1=self.k1)
         self.get_bias()
+
+    def get_future(self,run,y0,y1):
+        self.fut = Run(self.model,self.region,run,y0,y1,k0=self.k0,k1=self.k1)
+        self.get_delta()
+        self.get_anom()
 
     def plot_region_cmip(self,ii,jj):
         plt.pcolormesh(self.lonc,self.latc,np.nansum(self.thkcello,axis=0),cmap='Blues',norm=mpl.colors.LogNorm(vmin=10,vmax=1e4))
@@ -49,7 +56,7 @@ class Region:
         #ax[2].plot(self.fut.S[:,jj,ii],self.fut.T[:,jj,ii],c='tab:red')
 
     def plot_delta(self):
-        fig,ax = plt.subplots(1,4,figsize=(4*4,5),sharex=True,sharey=True)
+        fig,ax = plt.subplots(1,5,figsize=(5*3.5,4.5),sharex=True,sharey=True)
 
         im = ax[0].pcolormesh(self.ref.Sb,self.ref.Tb,self.ref.Vb.T,norm=mpl.colors.LogNorm(vmin=1e9,vmax=1e13))
         plt.colorbar(im,ax=ax[0],orientation='horizontal')
@@ -63,36 +70,39 @@ class Region:
 
         #im = ax[2].pcolormesh(self.prd.Sc,self.prd.Tc,self.fut.Vb.T,norm=mpl.colors.LogNorm(vmin=1e9,vmax=1e13),cmap='Greys')
         im = ax[2].pcolormesh(self.prd.Sb,self.prd.Tb,self.fut.deltaT.T,cmap='cmo.balance',vmin=-3,vmax=3)
-        #im = ax[2].pcolormesh(self.prd.Sc,self.prd.Tc,self.fut.deltaTf.T,cmap='cmo.balance',vmin=-2,vmax=2)
-
         plt.colorbar(im,ax=ax[2],orientation='horizontal')
         ax[2].set_title(f'{self.fut.run} ({self.fut.y0}-{self.fut.y1})')
+
+        im = ax[3].pcolormesh(self.prd.Sb,self.prd.Tb,self.fut.deltaTf.T,cmap='cmo.balance',vmin=-3,vmax=3)
+
+        plt.colorbar(im,ax=ax[3],orientation='horizontal')
+        ax[3].set_title(f'Filled')
         #ax[1].plot(self.fut.S[:,jj,ii],self.fut.T[:,jj,ii],c='tab:red')
 
-        im = ax[3].pcolormesh(self.ref.Sb,self.ref.Tb,self.ref.dTb.T,cmap='cmo.balance',vmin=-3,vmax=3)
-        plt.colorbar(im,ax=ax[3],orientation='horizontal')
+        im = ax[4].pcolormesh(self.ref.Sb,self.ref.Tb,self.fut.dTcorb.T,cmap='cmo.balance',vmin=-3,vmax=3)
+        plt.colorbar(im,ax=ax[4],orientation='horizontal')
         #ax[3].set_title(f'{self.fut.run} ({self.fut.y0}-{self.fut.y1})')
 
-    def plot_profiles(self,vlim=-10):
+    def plot_profiles(self,vlim=-1):
         fig,ax = plt.subplots(1,3,figsize=(10,5),sharey=True)
 
         Tav = np.average(np.where(np.isnan(self.ref.T[:vlim,:,:]),0,self.ref.T[:vlim,:,:]),axis=(1,2),weights=self.ref.V[:vlim,:,:])
-        ax[1].plot(Tav,self.ref.lev[:vlim,0],label='optimised',c='tab:red',ls='--')
+        ax[1].plot(Tav,self.ref.depth[:vlim],label='optimised',c='tab:red',ls='--')
 
         Tav = np.average(np.where(np.isnan(self.prd.T[:vlim,:,:]),0,self.prd.T[:vlim,:,:]),axis=(1,2),weights=self.prd.V[:vlim,:,:])
-        ax[0].plot(Tav,self.prd.lev[:vlim,0],label='raw',c='.5',ls='--')
+        ax[0].plot(Tav,self.prd.depth[:vlim],label='raw',c='.5',ls='--')
 
-        Tav = np.average(np.where(np.isnan(self.ref.T[:vlim,:,:]),0,self.ref.dT[:vlim,:,:]),axis=(1,2),weights=self.ref.V[:vlim,:,:])
-        ax[2].plot(Tav,self.ref.lev[:vlim,0],c='tab:red')
+        Tav = np.average(np.where(np.isnan(self.ref.T[:vlim,:,:]),0,self.fut.dTcor[:vlim,:,:]),axis=(1,2),weights=self.ref.V[:vlim,:,:])
+        ax[2].plot(Tav,self.ref.depth[:vlim],c='tab:red')
 
-        Tav = np.average(np.where(np.isnan(self.prd.T[:vlim,:,:]),0,(self.fut.T-self.prd.T)[:vlim,:,:]),axis=(1,2),weights=self.prd.V[:vlim,:,:])
-        ax[2].plot(Tav,self.prd.lev[:vlim,0],c='.5')
+        Tav = np.average(np.where(np.isnan(self.prd.T[:vlim,:,:]),0,self.fut.dTraw[:vlim,:,:]),axis=(1,2),weights=self.prd.V[:vlim,:,:])
+        ax[2].plot(Tav,self.prd.depth[:vlim],c='.5')
 
-        Tav = np.average(np.where(np.isnan(self.ref.T[:vlim,:,:]),0,(self.ref.T+self.ref.dT)[:vlim,:,:]),axis=(1,2),weights=self.ref.V[:vlim,:,:])
-        ax[1].plot(Tav,self.ref.lev[:vlim,0],c='tab:red')
+        Tav = np.average(np.where(np.isnan(self.ref.T[:vlim,:,:]),0,(self.ref.T+self.fut.dTcor)[:vlim,:,:]),axis=(1,2),weights=self.ref.V[:vlim,:,:])
+        ax[1].plot(Tav,self.ref.depth[:vlim],c='tab:red')
 
         Tav = np.average(np.where(np.isnan(self.prd.T[:vlim,:,:]),0,self.fut.T[:vlim,:,:]),axis=(1,2),weights=self.prd.V[:vlim,:,:])
-        ax[0].plot(Tav,self.prd.lev[:vlim,0],c='.5')
+        ax[0].plot(Tav,self.prd.depth[:vlim],c='.5')
 
         ax[0].set_title('Raw model')
         ax[1].set_title('Optimised')
@@ -100,8 +110,8 @@ class Region:
         #ax[0].legend()
 
         for Ax in ax:
-            Ax.set_ylim([0,1500])
-            Ax.invert_yaxis()
+            Ax.set_ylim([-1500,0])
+            #Ax.invert_yaxis()
 
     def get_woa(self):
         self.woa = Run('woa',1991,2020)
@@ -172,16 +182,6 @@ class Region:
         print(f'got variables of {run} run from year {y0} to {y1}')
         
         return out
-    
-    def get_delta(self):
-
-        VTdel,Sdel,Tdel = np.histogram2d(self.prd.S.flatten()[self.prd.V.flatten()>0],self.prd.T.flatten()[self.prd.V.flatten()>0],bins=100,weights=(self.prd.V*(self.fut.T-self.prd.T)).flatten()[self.prd.V.flatten()>0])
-        self.fut.deltaT = VTdel/self.prd.Vb
-        VSdel,Sdel,Tdel = np.histogram2d(self.prd.S.flatten()[self.prd.V.flatten()>0],self.prd.T.flatten()[self.prd.V.flatten()>0],bins=100,weights=(self.prd.V*(self.fut.S-self.prd.S)).flatten()[self.prd.V.flatten()>0])
-        self.fut.deltaS = VSdel/self.prd.Vb
-
-        self.fut.deltaTf = self.fill_delta(self.fut.deltaT)
-        self.fut.deltaSf = self.fill_delta(self.fut.deltaS)
 
     def get_bias(self,Tperc=.99,Sperc=.99):
 
@@ -197,12 +197,14 @@ class Region:
         self.prd.S95 = B[aa]
 
         # S-bias
-        a = np.arange(.5,1.5,.01)
+        a = np.arange(.7,1.3,.01)
         ref,dum = np.histogram(self.ref.S,bins=self.ref.Sb,weights=self.ref.V)
         rmse = np.zeros((len(a)))
         for A,AA in enumerate(a):
             prd,dum = np.histogram(AA*(self.prd.S-self.prd.S95)+self.ref.S95,bins=self.ref.Sb,weights=self.prd.V)
             rmse[A] = np.sum(np.where(prd==0,0,(np.log10(prd/np.sum(prd))-np.log10(ref/np.sum(ref))))**2)**.5 / np.sum(np.where(prd==0,0,1))
+            #rmse[A] = np.sum(np.where(prd==0,0,(np.log10(prd/np.sum(prd))-np.log10(ref/np.sum(ref))))**2) / np.sum(np.where(prd==0,0,1))**.5
+
             #print(AA,rmse[A])
         out = np.unravel_index(np.nanargmin(rmse, axis=None), rmse.shape)
         self.dSa = a[out[0]]
@@ -222,7 +224,20 @@ class Region:
         self.dTa = self.ref.TF95/self.prd.TF95
         self.prd.Tc = self.dTa*(self.prd.Tb-self.prd.Tb[0])+self.ref.Tb[0]
         #self.fut.Tc = self.dTa*(self.fut.Tb-self.fut.Tb[0])+self.ref.Tb[0]
-        
+
+    def get_delta(self):
+
+        self.fut.dTraw = self.fut.T-self.prd.T
+        self.fut.dSraw = self.fut.S-self.prd.S
+
+        VTdel,Sdel,Tdel = np.histogram2d(self.prd.S.flatten()[self.prd.V.flatten()>0],self.prd.T.flatten()[self.prd.V.flatten()>0],bins=100,weights=(self.prd.V*self.fut.dTraw).flatten()[self.prd.V.flatten()>0])
+        self.fut.deltaT = VTdel/self.prd.Vb
+        VSdel,Sdel,Tdel = np.histogram2d(self.prd.S.flatten()[self.prd.V.flatten()>0],self.prd.T.flatten()[self.prd.V.flatten()>0],bins=100,weights=(self.prd.V*self.fut.dSraw).flatten()[self.prd.V.flatten()>0])
+        self.fut.deltaS = VSdel/self.prd.Vb
+
+        self.fut.deltaTf = self.fill_delta(self.fut.deltaT)
+        self.fut.deltaSf = self.fill_delta(self.fut.deltaS)
+
     def fill_delta(self,deltavar):
         '''Routine to fill deltaT and deltaS in full normalised T,S space'''
 
@@ -243,6 +258,7 @@ class Region:
             newval = newval4
             newval[0,:] = np.nan
             newval[:,0] = np.nan
+
             Nleft = np.sum(np.isnan(newval[1:,1:]))
 
         return newval[1:,1:]
@@ -250,27 +266,44 @@ class Region:
     def get_anom(self):
         '''Get anomalies of reference T,S values'''
 
-        self.ref.dT = np.zeros(self.ref.T.shape)
-        self.ref.dS = np.zeros(self.ref.S.shape)
+        self.fut.dTcor = np.zeros(self.ref.T.shape)
+        self.fut.dScor = np.zeros(self.ref.S.shape)
 
-        for i in range(self.ref.dT.shape[0]):
-            for j in range(self.ref.dT.shape[1]):
-                for k in range(self.ref.dT.shape[2]):
+        for i in range(self.fut.dTcor.shape[0]):
+            for j in range(self.fut.dTcor.shape[1]):
+                for k in range(self.fut.dTcor.shape[2]):
                     if self.ref.V[i,j,k]==0:
                         continue
                     else:
                         isref = min(99,max(0,int(99*(self.ref.S[i,j,k]-self.prd.Sc[0])/(self.prd.Sc[-1]-self.prd.Sc[0]))))
                         jtref = min(99,max(0,int(99*(self.ref.T[i,j,k]-self.prd.Tc[0])/(self.prd.Tc[-1]-self.prd.Tc[0]))))
-                        self.ref.dS[i,j,k] = self.fut.deltaSf[isref,jtref]
-                        self.ref.dT[i,j,k] = self.fut.deltaTf[isref,jtref]
+                        self.fut.dScor[i,j,k] = self.fut.deltaSf[isref,jtref]
+                        self.fut.dTcor[i,j,k] = self.fut.deltaTf[isref,jtref]
 
         #Distributions of dS and dT, only for plotting purposes
-        VTdel,Sdel,Tdel = np.histogram2d(self.ref.S.flatten()[self.ref.V.flatten()>0],self.ref.T.flatten()[self.ref.V.flatten()>0],bins=100,range=[[self.ref.Sb[0],self.ref.Sb[-1]],[self.ref.Tb[0],self.ref.Tb[-1]]],weights=(self.ref.V*self.ref.dT).flatten()[self.ref.V.flatten()>0])
-        self.ref.dTb = VTdel/self.ref.Vb
-        VSdel,Sdel,Tdel = np.histogram2d(self.ref.S.flatten()[self.ref.V.flatten()>0],self.ref.S.flatten()[self.ref.V.flatten()>0],bins=100,range=[[self.ref.Sb[0],self.ref.Sb[-1]],[self.ref.Tb[0],self.ref.Tb[-1]]],weights=(self.ref.V*self.ref.dS).flatten()[self.ref.V.flatten()>0])
-        self.ref.dSb = VSdel/self.ref.Vb
+        VTdel,Sdel,Tdel = np.histogram2d(self.ref.S.flatten()[self.ref.V.flatten()>0],self.ref.T.flatten()[self.ref.V.flatten()>0],bins=100,range=[[self.ref.Sb[0],self.ref.Sb[-1]],[self.ref.Tb[0],self.ref.Tb[-1]]],weights=(self.ref.V*self.fut.dTcor).flatten()[self.ref.V.flatten()>0])
+        self.fut.dTcorb = VTdel/self.ref.Vb
+        VSdel,Sdel,Tdel = np.histogram2d(self.ref.S.flatten()[self.ref.V.flatten()>0],self.ref.S.flatten()[self.ref.V.flatten()>0],bins=100,range=[[self.ref.Sb[0],self.ref.Sb[-1]],[self.ref.Tb[0],self.ref.Tb[-1]]],weights=(self.ref.V*self.fut.dScor).flatten()[self.ref.V.flatten()>0])
+        self.fut.dScorb = VSdel/self.ref.Vb
 
-    def get_future(self,run,y0,y1):
-        self.fut = Run(self.model,self.region,run,y0,y1)#'ssp585',2081,2101)
-        self.get_delta()
-        self.get_anom()
+    def get_profiles(self):
+
+        self.ref.Tz = np.nan*np.ones(len(self.ref.depth))
+        self.ref.Sz = np.nan*np.ones(len(self.ref.depth))
+
+        kmax = np.where(np.sum(self.ref.V,axis=(1,2))==0)[0][0]
+        self.ref.Tz[:kmax] = np.average(np.where(np.isnan(self.ref.T[:kmax,:,:]),0,self.ref.T[:kmax,:,:]),axis=(1,2),weights=self.ref.V[:kmax,:,:])
+        self.ref.Sz[:kmax] = np.average(np.where(np.isnan(self.ref.S[:kmax,:,:]),0,self.ref.S[:kmax,:,:]),axis=(1,2),weights=self.ref.V[:kmax,:,:])
+
+    def calc_horav(self,var,run):
+        '''Calculate horizontally averaged vertical profile'''
+
+        try:
+            out = np.average(np.where(np.isnan(var),0,var),axis=(1,2),weights=run.V)
+        except:
+            out = np.nan*np.ones(var.shape[0])
+            kmax = np.where(np.sum(run.V,axis=(1,2))==0)[0][0]
+            out[:kmax] = np.average(np.where(np.isnan(var[:kmax,:,:]),0,var[:kmax,:,:]),axis=(1,2),weights=run.V[:kmax,:,:])
+
+        return out
+    
--- src/run.py
+++ src/run.py
@@ -33,6 +33,8 @@ class Run:
                 self.i0,self.i1,self.j0,self.j1 = 170,190,50,70
             elif self.model == 'EC-Earth3':
                 self.i0,self.i1,self.j0,self.j1 = 170,190,12,32
+            elif self.model == 'WOA23':
+                self.i0,self.i1,self.j0,self.j1 = -116,-96,-76,-70
         elif self.region == 'Ross':
             if self.model == 'CESM2':
                  self.i0,self.i1,self.j0,self.j1 = 180,220,2,7
@@ -40,44 +42,65 @@ class Run:
                 self.i0,self.i1,self.j0,self.j1 = 85,140,38,48
             elif self.model == 'EC-Earth3':
                 self.i0,self.i1,self.j0,self.j1 = 85,140,0,10           
-            
+            elif self.model == 'WOA23':
+                self.i0,self.i1,self.j0,self.j1 = -180,-150,-79,-76
+        elif self.region == 'Weddell':
+            if self.model == 'CESM2':
+                 self.i0,self.i1,self.j0,self.j1 = 300,360,3,9
+            elif self.model == 'UKESM1-0-LL':
+                self.i0,self.i1,self.j0,self.j1 = 224,246,38,55
+            elif self.model == 'EC-Earth3':
+                self.i0,self.i1,self.j0,self.j1 = 226,248,0,17
+            elif self.model == 'WOA23':
+                self.i0,self.i1,self.j0,self.j1 = -65,-40,-78.5,-74.5
+        elif self.region == 'Totten':
+            if self.model == 'CESM2':
+                 self.i0,self.i1,self.j0,self.j1 = 137,146,24,28
+            elif self.model == 'UKESM1-0-LL':
+                self.i0,self.i1,self.j0,self.j1 = 40,50,78,84
+            elif self.model == 'EC-Earth3':
+                self.i0,self.i1,self.j0,self.j1 = 42,52,40,46  
+            elif self.model == 'WOA23':
+                self.i0,self.i1,self.j0,self.j1 = 113,124,-67.5,-64.5
+
     def get_volume(self):
         if self.model in ['CESM2']:
             ds = xr.open_mfdataset(f'/usr/people/lambert/work2/data/cmip6/{self.model}/volcello*.nc')
             
             ds = ds.sel(nlat=slice(self.j0,self.j1),nlon=slice(self.i0,self.i1),lev=slice(self.k0*100.,self.k1*100.))
 
-            self.lev = ds.lev_bnds.values#/100.
+            self.lev = ds.lev_bnds.values
+            self.depth = -np.average(self.lev,axis=1)
             self.lon = ds.lon.values
             self.lat = ds.lat.values
-            #print('longitude:',np.min(self.lon),np.max(self.lon))
-            #print('latitude:',np.min(self.lat),np.max(self.lat))
             self.V = ds.volcello.values
             ds.close()
 
             #Mask
             ds = xr.open_mfdataset(f'/usr/people/lambert/work2/data/cmip6/{self.model}/historical/thetao/thetao*.nc')
-            ds = ds.sel(nlat=slice(self.j0,self.j1),nlon=slice(self.i0,self.i1))
+            ds = ds.sel(nlat=slice(self.j0,self.j1),nlon=slice(self.i0,self.i1),lev=slice(self.k0*100.,self.k1*100.))
             ds = ds.isel(time=0)
             self.V = np.where(np.isnan(ds.thetao),0,self.V)
             ds.close()
 
-        elif self.model in ['EC-Earth3','UKESM1-0-LL']:
+        elif self.model in ['EC-Earth3']:
 
-            ds = xr.open_mfdataset(f'/usr/people/lambert/work2/data/cmip6/{self.model}/{self.run}/thkcello/*.nc')
+            if self.y1-self.y0==1:
+                ds = xr.open_dataset(f'/usr/people/lambert/work2/data/cmip6/{self.model}/{self.run}/thkcello/thkcello_Omon_{self.model}_{self.run}_r1i1p1f1_gn_{self.y0}01-{self.y0}12.nc')
+            else:
+                ds = xr.open_mfdataset(f'/usr/people/lambert/work2/data/cmip6/{self.model}/{self.run}/thkcello/*.nc',combine='by_coords')
             ds = ds.sel(time=slice(f'{self.y0}-01-01',f'{self.y1}-01-01'),j=slice(self.j0,self.j1),i=slice(self.i0,self.i1),lev=slice(self.k0,self.k1))
-            #print(ds)
-            self.lev = np.average(ds.lev_bnds.values,axis=0,weights=dpm(self.y1-self.y0))
+            if self.y1-self.y0==1:
+                self.lev = ds.lev_bnds.values
+            else:
+                self.lev = np.average(ds.lev_bnds.values,axis=0,weights=dpm(self.y1-self.y0))
+            self.depth = -np.average(self.lev,axis=1)
             self.lon = ds.longitude.values
             self.lat = ds.latitude.values
-            #print('longitude:',np.min(self.lon),np.max(self.lon))
-            #print('latitude:',np.min(self.lat),np.max(self.lat))
-            #self.thkcello = ds.thkcello
             self.thkcello = np.average(ds.thkcello,axis=0,weights=dpm(self.y1-self.y0))
-            #self.thkcello = np.sum(ds.thkcello*dpm(self.y1-self.y0)[:,np.newaxis,np.newaxis,np.newaxis],axis=0)/np.sum(dpm(self.y1-self.y0))
             ds.close()
 
-            ds = xr.open_mfdataset(f'/usr/people/lambert/work2/data/cmip6/{self.model}/areacello*.nc')
+            ds = xr.open_dataset(f'/usr/people/lambert/work2/data/cmip6/{self.model}/areacello_Ofx_EC-Earth3_historical_r1i1p1f1_gn.nc')
             ds = ds.sel(j=slice(self.j0,self.j1),i=slice(self.i0,self.i1))
 
             self.areacello = ds.areacello.values
@@ -85,8 +108,51 @@ class Run:
 
             self.V = self.thkcello*self.areacello
             self.V = np.where(np.isnan(self.V),0,self.V)
+        
+        elif self.model in ['UKESM1-0-LL']:
+
+            ds = xr.open_mfdataset(f'/usr/people/lambert/work2/data/cmip6/{self.model}/{self.run}/thkcello/*.nc')
+            ds = ds.sel(time=slice(f'{self.y0}-01-01',f'{self.y1}-01-01'),j=slice(self.j0,self.j1),i=slice(self.i0,self.i1),lev=slice(self.k0,self.k1))
+            self.lev = np.average(ds.lev_bnds.values,axis=0,weights=dpm(self.y1-self.y0))
+            self.depth = -np.average(self.lev,axis=1)
+            self.lon = ds.longitude.values
+            self.lat = ds.latitude.values
+            self.thkcello = np.average(ds.thkcello,axis=0,weights=dpm(self.y1-self.y0))
+            ds.close()
+
+            ds = xr.open_dataset(f'/usr/people/lambert/work2/data/cmip6/{self.model}/areacello_Ofx_UKESM1-0-LL_piControl_r1i1p1f2_gn.nc')
+            ds = ds.sel(j=slice(self.j0,self.j1),i=slice(self.i0,self.i1))
+
+            self.areacello = ds.areacello.values
+            ds.close()
+
+            self.V = self.thkcello*self.areacello
+            self.V = np.where(np.isnan(self.V),0,self.V)
+        
+        elif self.model == 'WOA23':
+            ds = xr.open_dataset('/usr/people/lambert/work/projects/data/woa23/woa23_decav91C0_t00_04.nc',decode_cf=False)
+            ds = ds.isel(time=0)
+            ds = ds.sel(lon=slice(self.i0,self.i1),lat=slice(self.j0,self.j1),depth=slice(self.k0,self.k1))
+
+            self.T = np.where(ds.t_an<1e30,ds.t_an,np.nan)
+
+            self.lon = ds.lon.values
+            self.lat = ds.lat.values
+            self.depth = -ds.depth
+
+            self.V = np.zeros((len(ds.depth),len(ds.lat),len(ds.lon)))
+            R = 6.371e6
+            A = np.ones((len(ds.lat),len(ds.lon)))
+            for l in range(len(ds.lat)):
+                dy = 2*np.pi*R*(ds.lat_bnds[l,1]-ds.lat_bnds[l,0])/360
+                dx = 2*np.pi*R*(ds.lon_bnds[:,1]-ds.lon_bnds[:,0])/360 * np.cos(2*np.pi*ds.lat[l]/360)
+                A[l,:] = dx*dy
+
+            for d in range(len(ds.depth)):
+                D = np.where(np.isnan(self.T[d,:,:]),0,ds.depth_bnds[d,1]-ds.depth_bnds[d,0])
+                self.V[d,:,:] = D*A
+            ds.close()
             
-        #print('got volume')
     def get_TS(self):
         if self.model in ['CESM2']:
             ds = xr.open_mfdataset(f'/usr/people/lambert/work2/data/cmip6/{self.model}/{self.run}/thetao/*.nc',combine='by_coords')
@@ -98,7 +164,25 @@ class Run:
             ds = ds.sel(time=slice(f'{self.y0}-01-01',f'{self.y1}-01-01'),nlat=slice(self.j0,self.j1),nlon=slice(self.i0,self.i1),lev=slice(self.k0*100.,self.k1*100.))
             self.S = np.average(ds.so,axis=0,weights=dpm(self.y1-self.y0))
             ds.close()
-        elif self.model in ['EC-Earth3','UKESM1-0-LL']:
+
+        elif self.model in ['EC-Earth3']:
+            if self.y1-self.y0==1:
+                ds = xr.open_dataset(f'/usr/people/lambert/work2/data/cmip6/{self.model}/{self.run}/thetao/thetao_Omon_{self.model}_{self.run}_r1i1p1f1_gn_{self.y0}01-{self.y0}12.nc')
+            else:
+                ds = xr.open_mfdataset(f'/usr/people/lambert/work2/data/cmip6/{self.model}/{self.run}/thetao/*.nc',combine='by_coords')
+            ds = ds.sel(time=slice(f'{self.y0}-01-01',f'{self.y1}-01-01'),j=slice(self.j0,self.j1),i=slice(self.i0,self.i1),lev=slice(self.k0,self.k1))
+            self.T = np.average(ds.thetao,axis=0,weights=dpm(self.y1-self.y0))
+            ds.close()
+
+            if self.y1-self.y0==1:
+                ds = xr.open_dataset(f'/usr/people/lambert/work2/data/cmip6/{self.model}/{self.run}/so/so_Omon_{self.model}_{self.run}_r1i1p1f1_gn_{self.y0}01-{self.y0}12.nc')
+            else:
+                ds = xr.open_mfdataset(f'/usr/people/lambert/work2/data/cmip6/{self.model}/{self.run}/so/*.nc',combine='by_coords')
+            ds = ds.sel(time=slice(f'{self.y0}-01-01',f'{self.y1}-01-01'),j=slice(self.j0,self.j1),i=slice(self.i0,self.i1),lev=slice(self.k0,self.k1))
+            self.S = np.average(ds.so,axis=0,weights=dpm(self.y1-self.y0))
+            ds.close()
+
+        elif self.model in ['UKESM1-0-LL']:
             ds = xr.open_mfdataset(f'/usr/people/lambert/work2/data/cmip6/{self.model}/{self.run}/thetao/*.nc',combine='by_coords')
             ds = ds.sel(time=slice(f'{self.y0}-01-01',f'{self.y1}-01-01'),j=slice(self.j0,self.j1),i=slice(self.i0,self.i1),lev=slice(self.k0,self.k1))
             self.T = np.average(ds.thetao,axis=0,weights=dpm(self.y1-self.y0))
@@ -107,6 +191,14 @@ class Run:
             ds = xr.open_mfdataset(f'/usr/people/lambert/work2/data/cmip6/{self.model}/{self.run}/so/*.nc',combine='by_coords')
             ds = ds.sel(time=slice(f'{self.y0}-01-01',f'{self.y1}-01-01'),j=slice(self.j0,self.j1),i=slice(self.i0,self.i1),lev=slice(self.k0,self.k1))
             self.S = np.average(ds.so,axis=0,weights=dpm(self.y1-self.y0))
+            ds.close()            
+
+        elif self.model == 'WOA23':
+            #Already got T
+            ds = xr.open_dataset('/usr/people/lambert/work/projects/data/woa23/woa23_decav91C0_s00_04.nc',decode_cf=False)
+            ds = ds.isel(time=0)
+            ds = ds.sel(lon=slice(self.i0,self.i1),lat=slice(self.j0,self.j1),depth=slice(self.k0,self.k1))
+            self.S = np.where(ds.s_an<1e30,ds.s_an,np.nan)
             ds.close()
 
     def get_bins(self):
